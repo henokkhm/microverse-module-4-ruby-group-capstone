f1a85621c16a1090682a2717115a9231dd22131a
Henok Kirubel <henokhm2@gmail.com>
2023-10-04 00:11:46 +0300

    Add tests for can_be_archived? method of Book class

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -3,8 +3,8 @@ require_relative '../classes/book'
 require_relative '../classes/item'
 
 describe Book do
-  describe 'initialize function' do
-    it 'takes publisher, cover_state, publish_date (Date object), archived string parameters and returns a Book object' do
+  describe 'initialize method' do
+    it 'takes publisher, cover_state, publish_date (Date object), archived parameters and returns a Book' do
       publish_date = Date.parse('2020/12/12')
       my_book = Book.new('Packt', 'good', publish_date, false)
 
@@ -14,6 +14,26 @@ describe Book do
       expect(my_book.cover_state).to eql('good')
       expect(my_book.publish_date).to eql(publish_date)
       expect(my_book.archived).to eql(false)
+    end
+  end
+
+  describe 'can_be_archived method' do
+    it 'returns true when publish date is older than 10 years' do
+      publish_date = Date.parse('2010/12/12')
+      my_book = Book.new('Packt', 'good', publish_date, false)
+      expect(my_book.can_be_archived?).to eql(true)
+    end
+
+    it 'returns true when cover state is bad' do
+      publish_date = Date.parse('2010/12/12')
+      my_book = Book.new('Packt', 'bad', publish_date, false)
+      expect(my_book.can_be_archived?).to eql(true)
+    end
+
+    it 'returns false when publish date is more recent than 10 years and cover state is not bad' do
+      publish_date = Date.parse('2020/12/12')
+      my_book = Book.new('Packt', 'good', publish_date, false)
+      expect(my_book.can_be_archived?).to eql(false)
     end
   end
 end
