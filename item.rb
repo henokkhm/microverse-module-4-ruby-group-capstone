fea1358b1f0936c8867a415e178c7d3b3b6b5269
Bleu-Yves-Sopoude <bleuyves@gmail.com>
2023-10-03 15:28:10 +0000

    Add method archived

--- item.rb
+++ item.rb
@@ -4,11 +4,16 @@ class Item
 
   def initialize(publish_date, archived)
     @id = Random.rand(1..10_000)
-    @publish_date = publish_date
+    @publish_date = publish_date || Date.today
     @archived = archived
     @genre = nil
     @author = nil
     @source = nil
     @label = nil
   end
+
+  def can_be_archived?
+    (Date.today.year - @publish_date.year) >= 10
+  end
+
 end
