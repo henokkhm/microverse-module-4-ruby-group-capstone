6d753b53899fe9b33105c81a0f21dbdd87d4c96e
Bleu-Yves-Sopoude <bleuyves@gmail.com>
2023-10-04 14:25:52 +0000

    Add can_be_archived

--- classes/music_album.rb
+++ classes/music_album.rb
@@ -1,4 +1,5 @@
 require_relative 'item'
+require 'date'
 
 class MusicAlbum < Item
   attr_reader :id, :on_spotify
@@ -6,5 +7,9 @@ class MusicAlbum < Item
   def initialize(publish_date, on_spotify, archived)
     super(publish_date, archived)
     @on_spotify = on_spotify
+  end
+
+  def can_be_archived?
+    (Date.today.year - @publish_date.year) >= 10 && @on_spotify == true
   end
 end
