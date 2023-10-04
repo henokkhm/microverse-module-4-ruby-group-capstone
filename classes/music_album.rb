01966fea3b024fb62042b113c98621912eac104d
Bleu-Yves-Sopoude <bleuyves@gmail.com>
2023-10-04 08:36:20 +0000

    Fix argument

--- classes/music_album.rb
+++ classes/music_album.rb
@@ -1,12 +1,10 @@
 require_relative 'item'
-require 'date'
 
 class MusicAlbum < Item
   attr_reader :id, :on_spotify
 
-  def initialize(publish_date, on_spotify)
-    super(publish_date)
-    @id = Random.rand(1...1000)
+  def initialize(publish_date, on_spotify, archived)
+    super(publish_date, archived)
     @on_spotify = on_spotify
   end
 end
