cfd83d990c10481abd53b98a7218e2dd3aaeb917
Henok Kirubel <henokhm2@gmail.com>
2023-10-05 00:38:06 +0300

    Add skeleton for ConsoleManager class

---
+++ lib/console_manager.rb
@@ -0,0 +1,29 @@
+class ConsoleManager
+  def initialize; end
+
+  # add a book
+
+  # list all books
+
+  # add a music album
+
+  # list all music albums
+
+  # add a movie
+
+  # list all movies
+
+  # add a game
+
+  # list of games
+
+  # list all genres
+
+  # list all labels
+
+  # list all authors
+
+  # list all sources
+
+  # exit app
+end
