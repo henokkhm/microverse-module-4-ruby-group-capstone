db149d8a42f0d66a240c3d500b217c0fedaafd58
Briton Otieno <ojwangbriton@gmail.com>
2023-10-06 00:25:27 +0300

    Update author class

--- classes/author.rb
+++ classes/author.rb
@@ -1,15 +1,23 @@
-class Author
-  attr_accessor :name, :items
+require_relative 'item'
 
-  def initialize(name)
-    @name = name
+class Author < Item
+  attr_accessor :first_name, :last_name, :items
+
+  def initialize(first_name, last_name)
+    super(nil, false)
+    @first_name = first_name
+    @last_name = last_name
     @items = []
+  end
+
+  def full_name
+    "#{first_name} #{last_name}"
   end
 
   def add_item(item)
     return unless item.can_be_archived?
 
     @items << item
-    item.set_author(self)
+    item.author = self
   end
 end
