bebc1eda02cca417cd627382055de632aec6b9e5
Briton Otieno <ojwangbriton@gmail.com>
2023-10-05 08:22:41 +0300

    Update the class for items to be arrays

--- classes/author.rb
+++ classes/author.rb
@@ -1,13 +1,18 @@
-require 'item'
+class Item
+  attr_accessor :title, :published_date, :archived, :items
 
-class Author
-  attr_accessor :name
-
-  def initialize(name)
-    @name = name
+  def initialize(title, published_date)
+    @title = title
+    @published_date = published_date
+    @archived = false
+    @items = []
   end
 
-  def add_item(item)
-    item.set_author(self)
+  def can_be_archived?
+    (Time.now - @published_date) > 10 * 365 * 24 * 60 * 60
+  end
+
+  def move_to_archive
+    @archived = true if can_be_archived?
   end
 end
