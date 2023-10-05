e00087d5dfa3d9af1db97ed773823eee98e8aa23
Henok Kirubel <henokhm2@gmail.com>
2023-10-06 00:47:43 +0300

    Add CREATE TABLE statement for Books

--- schema.sql
+++ schema.sql
@@ -6,3 +6,14 @@ CREATE TABLE items(
     FOREIGN KEY (author_id) REFERENCES authors(id),
     FOREIGN KEY (label_id) REFERENCES labels(id)
 );
+
+CREATE TABLE books(
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    publisher VARCHAR(50),
+    cover_state VARCHAR(50),
+    publish_date DATE,
+    archived boolean,
+    FOREIGN KEY (genre_id) REFERENCES genres(id),
+    FOREIGN KEY (author_id) REFERENCES authors(id),
+    FOREIGN KEY (label_id) REFERENCES labels(id)
+);
