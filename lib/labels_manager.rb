4647b0b69a1fb0ba0f1b9e0600dcee27f2cee974
Henok Kirubel <henokhm2@gmail.com>
2023-10-05 17:41:28 +0300

    Add methods to save and load Labels from .json file

--- lib/labels_manager.rb
+++ lib/labels_manager.rb
@@ -1,3 +1,4 @@
+require 'json'
 require_relative '../classes/label'
 
 class LabelsManager
@@ -11,5 +12,22 @@ class LabelsManager
     label = Label.new(title, color)
     @labels_list.push(label)
     label
+  end
+
+  def save_to_file
+    labels_data = @labels_list.map(&:to_hash)
+    File.open('labels.json', 'w') do |file|
+      file.puts JSON.generate(labels_data)
+    end
+  end
+
+  def load_from_file
+    return unless File.exist?('labels.json')
+
+    label_data = JSON.parse(File.read('labels.json'))
+    label_data.each do |label_hash|
+      label = add_label(label_hash['title'], label_hash['color'])
+      label.id = label_hash['id']
+    end
   end
 end
