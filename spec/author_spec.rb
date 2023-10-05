f9300acdf459e0b52a1119ac50b3e70839f7b330
Briton Otieno <ojwangbriton@gmail.com>
2023-10-05 14:48:31 +0300

    Fix rubocop offenses

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -1,20 +1,23 @@
-require_relative '../classes/author.rb'  # Assuming your Author class is in an 'author.rb' file
+require_relative '../classes/author'
 
-# Describe the Author class and its behavior
 describe Author do
-  let(:author) { Author.new("Stephen King") }
+  let(:author) { Author.new('Stephen King') }
 
-  it "has a name attribute" do
-    expect(author.name).to eq("Stephen King")
+  it "can add items to the author's collection when they can be archived" do
+    item = double('item', can_be_archived?: true)
+    allow(item).to receive(:set_author)
+
+    author.add_item(item)
+
+    expect(author.items).to include(item)
   end
 
-  it "can add an item to the author's collection" do
-    # Create a sample item
-    item = double("Item")
+  it "does not add items to the author's collection when they cannot be archived" do
+    item = double('item', can_be_archived?: false)
+    allow(item).to receive(:set_author)
 
-    expect(item).to receive(:set_author).with(author)
-
-    # Call the add_item method
     author.add_item(item)
+
+    expect(author.items).not_to include(item)
   end
 end
