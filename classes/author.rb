ce271733a2738313c7b7e77b8162a741c494c2ca
Briton Otieno <ojwangbriton@gmail.com>
2023-10-05 08:44:32 +0300

    Update my tests

--- classes/author.rb
+++ classes/author.rb
@@ -1,13 +1,15 @@
-require_relative 'item'
-
 class Author
-  attr_accessor :name
+  attr_accessor :name, :items
 
   def initialize(name)
     @name = name
+    @items = []
   end
 
   def add_item(item)
-    item.set_author(self)
+    if item.can_be_archived?
+      @items << item
+      item.set_author(self)
+    end
   end
 end
