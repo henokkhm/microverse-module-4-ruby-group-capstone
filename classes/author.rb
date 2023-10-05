65f922235b2e51caf25a6d479678ef4e7f1b8931
Briton Otieno <ojwangbriton@gmail.com>
2023-10-06 01:18:49 +0300

    Update author.rb

--- classes/author.rb
+++ classes/author.rb
@@ -3,7 +3,7 @@ require_relative 'item'
 class Author < Item
   attr_accessor :first_name, :last_name, :items
 
-  def initialize(first_name, last_name, publish_date: nil, archived: false)
+  def initialize(first_name, last_name, publish_date = nil, archived = false)
     super(publish_date, archived)
     @first_name = first_name
     @last_name = last_name
