fb41f612818daa82d8e84d0a0c56e6afea89673c
Briton Otieno <ojwangbriton@gmail.com>
2023-10-06 16:44:10 +0300

    Save author to json

--- lib/author_manager.rb
+++ lib/author_manager.rb
@@ -12,4 +12,16 @@ class AuthorManager
     @authors << author
     author
   end
+
+  def save_authors_to_json(filename)
+    authors_data = @authors.map(&:to_hash)
+    File.open(filename, 'w') do |file|
+      file.puts JSON.pretty_generate(authors_data)
+    end
+  end
+
+  def load_authors_from_json(filename)
+    authors_data = JSON.parse(File.read(filename))
+    @authors = authors_data.map { |data| Author.from_hash(data) }
+  end
 end
